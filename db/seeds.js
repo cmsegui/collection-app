b4015c19a50d3af57de2e2b9de95f3620e3cd170
cmsegui <cmsegui@yahoo.com>
2017-08-12 17:37:27 -0400

    added another user to seeds file

--- db/seeds.js
+++ db/seeds.js
@@ -249,6 +249,231 @@ const shoe15 = new Shoe(
     }
 )
 
+const shoe16 = new Shoe(
+    {
+        name: 'Nike Air Jordan 11 BLACKOUT',
+        year: 2012,
+        description: 'The most expenseive sneaker out of the box, most people thought this would be a sample of a later release but it was the only one.',
+        released: 1,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/a3sHaoX.jpg'
+    }
+)
+
+const shoe17 = new Shoe(
+    {
+        name: 'Nike Dunk High LE WU-TANG',
+        year: 1999,
+        description: 'Rumor has it these were only made for friends and family of Wu-Tang.',
+        released: 36,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/ukHUyE9.jpg'
+    }
+)
+
+const shoe18 = new Shoe(
+    {
+        name: 'Nike Air Yeezy 1 Grammy Prototype',
+        year: 2008,
+        description: 'Kanye wore these to the Grammys when his partnership with Nike was announced.',
+        released: 1,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/mTxX9IB.jpg'
+    }
+)
+
+const shoe19 = new Shoe(
+    {
+        name: 'Nike Air Jordan 3',
+        year: 1988,
+        description: 'The OG of Jordans, these still have the tags on them.',
+        released: 5000,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/n8QkPwV.jpg'
+    }
+)
+
+const shoe20 = new Shoe(
+    {
+        name: 'Nike Dunk SB High FLOM',
+        year: 2005,
+        description: 'Aside from 3 pairs that were rafflied off to the fiends, these were exclusively for friends and family of FUTURA.',
+        released: 24,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/dcFGVxw.jpg'
+    }
+)
+
+const shoe21 = new Shoe(
+    {
+        name: 'Gold-Dipped Nike Dunks',
+        year: 2005,
+        description: 'Designed by artist Kenneth Courtney, these were dipped in 24K gold.',
+        released: 5,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/7ps7dt0.jpg'
+    }
+)
+
+const shoe22 = new Shoe(
+    {
+        name: 'UNDFTD x Air Jordan 4',
+        year: 2005,
+        description: 'Jordans first collaboration.',
+        released: 79,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/k92tBvY.jpg'
+    }
+)
+
+const shoe23 = new Shoe(
+    {
+        name: 'SF x PUMA SharkBait',
+        year: 2008,
+        description: 'Made with sharkskin leather, this was only released to friends and family of SneakerFreaker.',
+        released: 30,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/KnOj5xw.jpg'
+    }
+)
+
+const shoe24 = new Shoe(
+    {
+        name: 'Avia 880',
+        year: 1989,
+        description: 'This shoe by a now defunct brand, was worn by 4 NBA Hall of Famers in the 80s.',
+        released: 5000,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/MuYCQyF.jpg'
+    }
+)
+
+const shoe25 = new Shoe(
+    {
+        name: 'Reebok The Pump',
+        year: 1989,
+        description: 'The OG Pump...everyone had a pair of Pumps.',
+        released: 50000,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/uoUR7Sq.jpg'
+    }
+)
+
+const shoe26 = new Shoe(
+    {
+        name: 'Nike Lebron South Beach 8',
+        year: 2010,
+        description: 'This was not only a new beginning for colorways but for Lebron making the move to Miami.',
+        released: 1000,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/GQyJFsm.jpg'
+    }
+)
+
+const shoe27 = new Shoe(
+    {
+        name: 'Nike Dunk Low Pro SB Heineken',
+        year: 2003,
+        description: 'The beer company wanted no part of this release.',
+        released: 5000,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/VLv7dfS.jpg'
+    }
+)
+
+const shoe28 = new Shoe(
+    {
+        name: 'BAPE x adidas Super Ape Star',
+        year: 2003,
+        description: 'A B-side version was released n 2011 but it did not compare to the OG.',
+        released: 5000,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/MQbMZFL.jpg'
+    }
+)
+
+const shoe29 = new Shoe(
+    {
+        name: 'Nike Dunk High Pro SB UNKLE',
+        year: 2004,
+        description: 'You may not remember UNKLE but you should remember that Futura designed these.',
+        released: 1000,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/JjrwH3o.jpg'
+    }
+)
+
+const shoe30 = new Shoe(
+    {
+        name: 'Nike HyperDunk McFly',
+        year: 2008,
+        description: 'Kobe Bryant autographed these out of a DeLorean to celebrate the release.',
+        released: 3000,
+        condition: {
+            type: '',
+            enum: ['NEW', 'LIKE NEW', 'VERY GOOD', 'GOOD', 'ACCEPTABLE'],
+            default: 'GOOD'
+        },
+        img: 'http://i.imgur.com/grWZu09.jpg'
+    }
+)
+
 const Shoebox1 = new Shoebox(
     {
         name: 'Vintage Kicks',
@@ -256,6 +481,16 @@ const Shoebox1 = new Shoebox(
         createdAt: Date.now(),
         updatedAt: Date.now(),
         shoes: [shoe1, shoe2, shoe3, shoe4, shoe5, shoe6, shoe7, shoe8, shoe9, shoe10, shoe11, shoe12, shoe13, shoe14, shoe15]
+    }
+)
+
+const Shoebox2 = new Shoebox(
+    {
+        name: 'For the Purists',
+        description: 'Nothing crazy just dope sneakers.' ,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        shoes: [shoe16, shoe17, shoe18, shoe19, shoe20, shoe21, shoe22, shoe23, shoe24, shoe25, shoe26, shoe27, shoe28, shoe29, shoe30]
     }
 )
 
@@ -268,12 +503,26 @@ const user1 = new User(
     }
 )
 
+const user2= new User(
+    {
+    userName: 'Jeffrey Lebowski',
+    email: 'thedude@email.com',
+    img: 'http://i.imgur.com/ooGKhIg.jpg',
+    shoebox: Shoebox2
+    }
+)
+
+
 user1.save().then(() => {
     console.log('user saved!');    
 }).catch((err) => {
     console.log('error saving user', err);
 });
 
-
+user2.save().then(() => {
+    console.log('user saved!');    
+}).catch((err) => {
+    console.log('error saving user', err);
+});
 
 mongoose.connection.close();
