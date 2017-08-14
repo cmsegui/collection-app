e8562a63bf54af462e591aabf01b46c4c1b7a7bd
cmsegui <cmsegui@yahoo.com>
2017-08-14 14:28:28 -0400

    added axios and styled components

---
+++ client/src/components/User.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+import styled from 'styled-components';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>THIS IS WORKING, I AM A GOD</h1>      
+      </div>
+    );
+  }
+}
+
+export default App;
