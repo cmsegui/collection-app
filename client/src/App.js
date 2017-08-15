dd3e6c95a06ac73abe9191b0188bf05cbdc42e15
cmsegui <cmsegui@yahoo.com>
2017-08-14 22:03:53 -0400

    got all crud routes for shoe working i AM A GOD

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import User from './components/User';
 
 
 class App extends Component {
