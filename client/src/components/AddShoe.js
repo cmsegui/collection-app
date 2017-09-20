d891e75c33194db8e1cdb7f2b4aca54b268e6f6f
cmsegui <cmsegui@yahoo.com>
2017-09-20 13:16:28 -0400

    began to add shoe

---
+++ client/src/components/AddShoe.js
@@ -0,0 +1,22 @@
+// import React, { Component } from 'react';
+// import axios from 'axios';
+// import { Redirect }  from 'react-router-dom';
+
+// class AddShoe extends Component {
+//     constructor() {
+//      super();
+//      this.state = {
+//         home: {
+//             name: '',
+//             year: 0,
+//             description: '',
+//             released: 0,
+//             condition: '',
+//             img: '',
+//         },
+//         redirect: false
+//        }
+//     }
+
+
+// export default AddShoe;
